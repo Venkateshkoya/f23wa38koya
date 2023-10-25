bd4f0b42d078b5188c05f5aaea1955220c9ae1d1
Venkateshkoya <venkateshkoya8@gmail.com>
2023-10-25 11:22:53 -0500

    add computation

---
+++ routes/computation.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+    let x = Math.random() * 100;
+    x = x.toFixed(3)
+    if (req.query.x != undefined){
+        x = parseFloat(req.query.x);
+    }
+    let num1 = Math.log2(x);
+    let num2 = Math.cosh(x);
+    let num3 = Math.floor(x);
+    res.render('computation',{x:x, log2:num1, cosh: num2, floor:num3});
+});
+module.exports = router;
